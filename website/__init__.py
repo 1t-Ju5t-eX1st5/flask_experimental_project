47347e50b2ef7aa7086b888c698a498bca24178c
IronForce-Auscent <discordgamer.070910@gmai.com>
2023-02-19 18:09:12 +0800

    Added some new features
    Added a basic database structure and a (relatively) simple login system

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
+    # Starts up the application
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'some-secret-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    
 
     from .views import views
     from .auth import auth
     
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    # Builds the database
+    from .models import User, Note
+    with app.app_context():
+        db.create_all()
 
     return app
